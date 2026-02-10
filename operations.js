7cee52e71935d516f1ec46cf5a56db748330a184
gomesleke <voyagerleke@gmail.com>
2026-02-10 15:37:23 -0300

    functions

---
+++ operations.js
@@ -0,0 +1,18 @@
+export function sum(n1,n2){
+    return n1+n2;
+};
+export function subtration(n1,n2){
+    return n1-n2;
+};
+export function multiplication(n1,n2){
+    return n1*n2;
+};
+export function disivion(n1,n2){
+    return n1/n2;
+};
+export function power(n1,n2){
+    return n1**n2;
+};
+export function root(n1,n2){
+    return Math.pow(n1,1/n2);
+};
